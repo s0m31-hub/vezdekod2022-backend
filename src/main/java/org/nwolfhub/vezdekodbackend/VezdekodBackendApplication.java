62b590fb261e09672398bf6287485f6a34b3b9ea
s0m31 <notlimitedwolf676@gmail.com>
2022-06-04 10:13:40 +0300

    Init project

---
+++ src/main/java/org/nwolfhub/vezdekodbackend/VezdekodBackendApplication.java
@@ -0,0 +1,13 @@
+package org.nwolfhub.vezdekodbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VezdekodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VezdekodBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nwolfhub/vezdekodbackend/VezdekodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nwolfhub.vezdekodbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VezdekodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
